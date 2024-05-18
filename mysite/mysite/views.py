886def29d6811bca42f7ff5e3329e4577df37b90
kulevkirill <kkulev7@gmail.com>
2024-05-19 02:03:14 +0300

    add return buttons to solved_task.html & add new tasks

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -12,32 +12,6 @@ def task_list(request):
     tasks = tasks_utils.get_tasks()
     return render(request, "task_list.html", context={"tasks": tasks})
 
-def add_task(request):
-    return render(request, "add_task.html")
-
-def send_task(request):
-    if request.method == "POST":
-        cache.clear()
-        user_name = request.POST.get("name")
-        new_task = request.POST.get("new_task", "").replace(";", ",")
-        new_answer = request.POST.get("new_answer", "") # def -> answer, term -> task
-        context = {"user": user_name}
-        if len(new_answer) == 0:
-            context["success"] = False
-            context["comment"] = "Описание должно быть не пустым"
-        elif len(new_task) == 0:
-            context["success"] = False
-            context["comment"] = "Термин должен быть не пустым"
-        else:
-            context["success"] = True
-            context["comment"] = "Ваш термин принят"
-            tasks_utils.write_task(new_task, new_answer)
-        if context["success"]:
-            context["success-title"] = ""
-        return render(request, "task_request.html", context)
-    else:
-        add_task(request)
-
 def solve_task(request):
     tasks = tasks_utils.get_tasks()
     task = tasks[random.randint(0, len(tasks)-1)]
@@ -56,3 +30,29 @@ def send_answer(request):
 
         context["success"] = False
         return render(request, "solved_task.html", context)
+
+def add_task(request):
+    return render(request, "add_task.html")
+
+def send_task(request):
+    if request.method == "POST":
+        cache.clear()
+        user_name = request.POST.get("name")
+        new_task = request.POST.get("new_task", "").replace(";", ",")
+        new_answer = request.POST.get("new_answer", "")
+        context = {"user": user_name}
+        if len(new_answer) == 0:
+            context["success"] = False
+            context["comment"] = "Описание должно быть не пустым"
+        elif len(new_task) == 0:
+            context["success"] = False
+            context["comment"] = "Термин должен быть не пустым"
+        else:
+            context["success"] = True
+            context["comment"] = "Ваш термин принят"
+            tasks_utils.write_task(new_task, new_answer)
+        if context["success"]:
+            context["success-title"] = ""
+        return render(request, "task_request.html", context)
+
+    add_task(request)
